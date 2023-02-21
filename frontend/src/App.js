a653f2bc8edd686fe829e9307565b24234e12ef5
Jazz Abad <abadjazzrell@gmail.com>
2023-02-21 17:33:42 +0800

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>ProTech</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
