2e5d19cf38c4645f7d22596612ebec53093d3c7d
Jazz Abad <abadjazzrell@gmail.com>
2023-02-21 21:51:24 +0800

    Updated Product Details Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import logger from "use-reducer-logger";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,6 +44,9 @@ function HomeScreen() {
 
   return (
     <div>
+      <Helmet>
+        <title>ProTech</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className='products'>
         {loading ? (
